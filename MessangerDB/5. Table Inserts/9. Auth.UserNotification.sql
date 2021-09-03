a178fbb29123003d31dcb64906512d37473a4477
samkelo-nhlapo <72704428+samkelo-nhlapo@users.noreply.github.com>
2021-09-03 16:06:07 +0200

    Stored Procedures basics

--- MessangerDB/5. Table Inserts/9. Auth.UserNotification.sql
+++ MessangerDB/5. Table Inserts/9. Auth.UserNotification.sql
@@ -14,4 +14,12 @@ VALUES
 ('User Contact details not successfully saved, please try re-enter again. If the problem continues, please contact support', 1, GETDATE()),
 ('No User Contact  details currently saved for ', 1, GETDATE()),
 ('Unable to get User Contact details, please try re-enter again. If the problem continues, please contact support.', 1, GETDATE()),
-('User Contact details successfully updated', 1, GETDATE())
+('User Contact details successfully updated', 1, GETDATE()),
+('User contact details already exists please try again. If the problem continues. Please contact customer support', 1, GETDATE()),
+
+--Address
+('User Address details saved successfully.', 1, GETDATE()),
+('User Address details not saved successfully, please try re-enter again. If the problem continues, please Address support', 1, GETDATE()),
+('No User Address details currently saved for ', 1, GETDATE()),
+('Unable to get User Address details, please try re-enter again. If the problem continues, please Address support.', 1, GETDATE()),
+('User Address details successfully updated', 1, GETDATE())
---
+++ MessangerDB/6. Store Procedure/4. Location.spSaveUserAddress.sql
@@ -0,0 +1,105 @@
+USE Messanger
+GO
+
+/*
+===============================================================================
+		Author:			  Samkelo Nhlapo
+		Create date:	  03/09/2021
+		Description:      Add insert into address data
+		TFS Task:	      Create stored procedures for Address
+================================================================================
+*/
+
+CREATE OR ALTER PROC Location.spSaveUserAddress
+(
+	@User UNIQUEIDENTIFIER,
+	@Street VARCHAR(200),
+	@CityID INT,
+	@PostalCode VARCHAR(10),
+	@ProvinceFK INT,
+	@CountryFK INT,
+	@Latitude NVARCHAR(MAX),
+	@Longitude NVARCHAR(MAX),
+	@Message VARCHAR(MAX) OUTPUT
+)
+AS
+BEGIN
+	
+	SET NOCOUNT ON
+	BEGIN TRAN
+
+	DECLARE @IsActive BIT = 1,
+			@DefaultDate DATETIME  = GETDATE(),
+			@GeoFK UNIQUEIDENTIFIER,
+			@AddressID UNIQUEIDENTIFIER,
+			@ErrorSchema VARCHAR(MAX),
+			@ErrorProc VARCHAR(MAX),
+			@ErrorNumber VARCHAR(MAX),
+			@ErrorState VARCHAR(MAX),
+			@ErrorSeverity VARCHAR(MAX),
+			@ErrorLine VARCHAR(MAX),
+			@ErrorMessage VARCHAR(MAX),
+			@ErrorDate DATETIME
+
+	IF NOT EXISTS(SELECT * FROM Location.Address AS PA JOIN Profile.Users AS PU ON PU.AddressIDFK = PA.AddressID WHERE PU.UserID = @User )
+	BEGIN
+		
+		INSERT INTO Location.Geo(Latitude,Longitude)
+		VALUES(@Latitude, @Longitude)
+
+		INSERT INTO Location.Address
+		(
+			Street,
+			CityIDFK,
+			PostalCode,
+			ProvinceIDFK,
+			GeoIDFK,
+			IsActive,
+			UpdateDate
+		)
+		VALUES(dbo.CapitalizeFirstLetter(@Street),@CityID, @PostalCode, @ProvinceFK, @CountryFK, @GeoFK, @IsActive, @DefaultDate)
+
+		SET @Message = (SELECT * FROM Auth.UserNotification WITH (NOLOCK) WHERE UserNotificationID = 7 )
+
+		COMMIT TRAN
+
+	END ELSE
+	BEGIN
+		
+		UPDATE Location.Geo SET 
+				Latitude = @Latitude,
+				Longitude = @Longitude
+		WHERE GeoId = @AddressID
+
+		UPDATE Location.Address SET 
+				Street = dbo.CapitalizeFirstLetter(@Street),
+				CityIDFK = @CityID,
+				PostalCode = @PostalCode,
+				ProvinceIDFK = @ProvinceFK,
+				CountryIDFK = @CountryFK,
+				GeoIDFK = @GeoFK,
+				IsActive = @IsActive,
+				UpdateDate = @DefaultDate
+		WHERE AddressID = @AddressID
+
+		SET @Message = (SELECT * FROM Auth.UserNotification WITH (NOLOCK) WHERE UserNotificationID = 7 )
+
+		COMMIT TRAN
+
+	END
+
+		SET @ErrorSchema = OBJECT_SCHEMA_NAME(@@PROCID)
+		SET @ErrorProc = OBJECT_NAME(@@PROCID)
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorState  = ERROR_STATE()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorLine = ERROR_LINE()
+		SET @ErrorMessage = ERROR_MESSAGE()
+		SET @ErrorDate = @DefaultDate
+
+		EXEC [Auth].[spLogExceptions] @ErrorSchema, @ErrorProc, @ErrorNumber, @ErrorState, @ErrorSeverity, @ErrorLine, @ErrorMessage, @ErrorDate, 8
+
+		SET @Message = (SELECT UserNotification FROM Auth.UserNotification WITH(NOLOCK) WHERE UserNotificationID = 8)
+
+
+END
