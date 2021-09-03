c346ff11f2d0ae95e5d95da861efe7bce08de66a
samkelo-nhlapo <72704428+samkelo-nhlapo@users.noreply.github.com>
2021-09-03 17:32:53 +0200

    stored procedures

---
+++ MessangerDB/6. Store Procedure/5. Profile.spSaveUserDetails.sql
@@ -0,0 +1,105 @@
+USE Messanger
+GO
+
+/*
+===============================================================================
+		Author:			  Samkelo Nhlapo
+		Create date:	  03/09/2021
+		Description:      Add insert into User data
+		TFS Task:	      Create stored procedures for User
+================================================================================
+*/
+
+CREATE OR ALTER PROC Profile.spSaveUserDetails
+(
+	@UserID UNIQUEIDENTIFIER,
+	@Name VARCHAR(250),
+	@DateOfBirth DATETIME,
+	@AddressFK UNIQUEIDENTIFIER,
+	@ContactsFK UNIQUEIDENTIFIER,
+	@UserAuthtypeFK INT,
+	@Message VARCHAR(MAX) OUTPUT
+)
+AS
+BEGIN
+	
+SET NOCOUNT ON 
+
+	DECLARE @DefaultDate DATETIME = GETDATE(),
+			@IsArchived BIT = 0,
+			@IsReported BIT = 0,
+			@IsBlocked BIT = 0,
+			@IsActive BIT = 1,
+			@ErrorSchema VARCHAR(MAX),
+			@ErrorProc VARCHAR(MAX),
+			@ErrorNumber VARCHAR(MAX),
+			@ErrorState VARCHAR(MAX),
+			@ErrorSeverity VARCHAR(MAX),
+			@ErrorLine VARCHAR(MAX),
+			@ErrorMessage VARCHAR(MAX),
+			@ErrorDate DATETIME
+	
+	BEGIN TRY
+		
+		IF NOT EXISTS(SELECT * FROM Profile.Users WHERE UserID = @UserID)
+		BEGIN
+
+			INSERT INTO Profile.Users
+			(
+				UserName, 
+				DateOfBirth, 
+				AddressIDFK,
+				ContactIDFK, 
+				UserAuthTypeIDFK, 
+				UserIsArchived, 
+				UserIsReported, 
+				UserIsBlocked, 
+				UserIsActive, 
+				CreatedDate, 
+				UpdateDate
+			)
+			VALUES(@Name, @DateOfBirth, @AddressFK, @ContactsFK, @UserAuthtypeFK, @IsArchived, @IsReported, @IsBlocked, @IsActive, @DefaultDate, @DefaultDate)
+
+			SET @Message = (SELECT * FROM Auth.UserNotification WHERE UserNotificationID = )
+
+		END ELSE
+		BEGIN
+
+			UPDATE Profile.Users SET
+			UserName = @Name,
+			DateOfBirth = @DateOfBirth,
+			AddressIDFK = @AddressFK,
+			ContactIDFK = @ContactsFK,
+			UserAuthTypeIDFK = @UserAuthtypeFK,
+			UserIsArchived = @IsArchived,
+			UserIsReported = @IsReported,
+			UserIsBlocked = @IsBlocked,
+			UserIsActive = @IsActive,
+			CreatedDate = @DefaultDate,
+			UpdateDate = @DefaultDate
+			WHERE UserID = @UserID
+
+			
+			SET @Message = (SELECT * FROM Auth.UserNotification WHERE UserNotificationID = )
+
+		END
+
+	END TRY
+	BEGIN CATCH
+		
+		SET @ErrorSchema = OBJECT_SCHEMA_NAME(@@PROCID)
+		SET @ErrorProc = OBJECT_NAME(@@PROCID)
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorState = ERROR_STATE()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorLine = ERROR_LINE()
+		SET @ErrorMessage = ERROR_MESSAGE()
+		
+
+		EXEC Auth.spLogExceptions @ErrorSchema,@ErrorProc,@ErrorNumber,@ErrorState,@ErrorSeverity,@ErrorLine,@ErrorMessage,@DefaultDate,
+		
+		SET @Message = (SELECT * FROM Auth.UserNotification WHERE UserNotificationID = )
+
+	END CATCH
+SET NOCOUNT OFF
+END
---
+++ MessangerDB/6. Store Procedure/6. Profile.spSaveUserProfile.sql
@@ -0,0 +1,81 @@
+USE Messanger
+GO
+
+/*
+===============================================================================
+		Author:			  Samkelo Nhlapo
+		Create date:	  03/09/2021
+		Description:      Add insert into Profile data
+		TFS Task:	      Create stored procedures for Profile
+================================================================================
+*/
+
+CREATE OR ALTER PROC Profile.spSaveUserProfile
+(
+	@UserID UNIQUEIDENTIFIER,
+	@DP UNIQUEIDENTIFIER,
+	@Status VARCHAR(50),
+	@Message VARCHAR(MAX) OUTPUT
+)
+AS
+BEGIN
+SET NOCOUNT ON
+	
+	DECLARE @DefaultDate DATETIME = GETDATE(),
+			@ErrorSchema VARCHAR(MAX),
+			@ErrorProc VARCHAR(MAX),
+			@ErrorNumber VARCHAR(MAX),
+			@ErrorState VARCHAR(MAX),
+			@ErrorSeverity VARCHAR(MAX),
+			@ErrorLine VARCHAR(MAX),
+			@ErrorMessage VARCHAR(MAX),
+			@ErrorDate DATETIME
+
+	BEGIN TRY
+
+	IF NOT EXISTS(SELECT * FROM Profile.Profile WHERE UserIDFK = @UserID)
+	BEGIN
+		
+		INSERT INTO Profile.Profile
+		(
+			UserIDFK, 
+			DisplayProfileIDFK, 
+			Status, 
+			Created_at, 
+			Updatedate
+		)
+		VALUES(@UserID, @DP, dbo.CapitalizeFirstLetter(@Status), @DefaultDate, @DefaultDate)
+
+		SET @Message = (SELECT * FROM Auth.UserNotification WHERE UserNotificationID =)
+
+	END ELSE
+	BEGIN
+
+		UPDATE Profile.Profile SET
+				UserIDFK = @UserID,
+				DisplayProfileIDFK = @DP,
+				Status = @Status,
+				Created_at = @DefaultDate,
+				Updatedate = @DefaultDate
+		WHERE UserIDFK = @UserID
+
+		SET @Message = (SELECT * FROM Auth.UserNotification WHERE UserNotificationID =)
+
+	END
+	END TRY
+	BEGIN CATCH
+
+		SET @ErrorSchema = OBJECT_SCHEMA_NAME(@@PROCID)
+		SET @ErrorProc = OBJECT_NAME(@@PROCID)
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorState = ERROR_STATE()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorLine = ERROR_LINE()
+		SET @ErrorMessage = ERROR_MESSAGE()
+
+		EXEC Auth.spLogExceptions @ErrorSchema,@ErrorProc,@ErrorNumber,@ErrorState,@ErrorSeverity,@ErrorLine,@ErrorMessage,@DefaultDate,
+
+		SET @Message = (SELECT * FROM Auth.UserNotification WHERE UserNotificationID =)
+	END CATCH
+SET NOCOUNT OFF
+END
