4285e768734a375ca7e570898650749a2b5a137c
Samkelo Michael <72704428+samkelo-nhlapo@users.noreply.github.com>
2022-01-22 22:48:14 +0200

    Save Contacts

--- .Net/MessengerVS/MessengerVS/Controllers/HomeController.cs
+++ .Net/MessengerVS/MessengerVS/Controllers/HomeController.cs
@@ -74,35 +74,36 @@ namespace MessengerVS.Controllers
             }
         }
 
-        //public JsonResult GetCities()
-        //{
-        //    using (var db = GetContxt())
-        //    {
-        //        try
-        //        {
-        //            var eventCities = db.Database.SqlQuery<LocationModel>(String.Format("Location.spGetCities")).ToList();
-        //            return new JsonResult { Data = eventCities,JsonRequestBehavior = JsonRequestBehavior.AllowGet };
-        //        }
-        //        catch (Exception)
-        //        {
+        public JsonResult GetCities()
+        {
+            using (var db = GetContxt())
+            {
+                try
+                {
+                    var eventCities = db.Database.SqlQuery<LocationModel>(String.Format("Location.spGetCities")).ToList();
+                    return new JsonResult { Data = eventCities,JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                }
+                catch (Exception)
+                {
 
-        //            throw;
-        //        }
-        //    }
-        //}
+                    throw;
+                }
+            }
+        }
 
         [HttpPost]
         public JsonResult SaveContacts(LocationModel locationModel)
         {
+
             using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MessengerEntity"].ConnectionString))
             {
-                SqlCommand cmd = new SqlCommand("[Contacts].[spAddContacts]", connection);
+                SqlCommand cmd = new SqlCommand("Contacts.spAddContacts", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 cmd.Parameters.Add(new SqlParameter("@PhoneNumber", locationModel.PhoneNumber));
-                cmd.Parameters.Add(new SqlParameter("@PhoneTypeID", Int32.Parse(locationModel.PhonetypeId)));
+                cmd.Parameters.Add(new SqlParameter("@PhoneTypeID", locationModel.PhonetypeId));
                 cmd.Parameters.Add(new SqlParameter("@Email", locationModel.Email));
-                cmd.Parameters.Add(new SqlParameter("@EmailType", Int32.Parse(locationModel.EmailType)));
+                cmd.Parameters.Add(new SqlParameter("@EmailType", locationModel.EmailType));
 
                 cmd.Parameters.Add(new SqlParameter("@Message", SqlDbType.VarChar, 250)).Direction = ParameterDirection.Output;
 
@@ -114,7 +115,8 @@ namespace MessengerVS.Controllers
 
                 connection.Close();
             }
-            return new JsonResult { Data = locationModel, JsonRequestBehavior=JsonRequestBehavior.AllowGet };
+            
+            return new JsonResult { Data = locationModel, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
         public static Contxt GetContxt()
---
+++ MessangerDB/6. Stored Procedure/003. [Contacts].[spAddContacts].sql
@@ -0,0 +1,121 @@
+USE [Messenger]
+GO
+
+/****** Object:  StoredProcedure [Contacts].[spAddContacts]    Script Date: 22-Jan-22 06:36:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+ALTER     PROC [Contacts].[spAddContacts]
+(
+	@PhoneNumber VARCHAR(12) = '',
+	@PhoneTypeID INT = 1,
+	@Email VARCHAR(250) = '',
+	@EmailType INT = 1,
+	@Message VARCHAR(250) OUTPUT
+)
+AS
+BEGIN
+
+SET NOCOUNT ON
+	
+	DECLARE @DefaultDate DATETIME = GETDATE(),
+			@PhoneID UNIQUEIDENTIFIER = NEWID(),
+			@EmailID UNIQUEIDENTIFIER = NEWID(),
+			@ContactID UNIQUEIDENTIFIER = NEWID(),
+			@ContactTypeID INT = 1,
+			@IsActive BIT = 0,
+			@ErrorSchema VARCHAR(MAX),
+			@ErrorProc VARCHAR(MAX),
+			@ErrorNumber VARCHAR(MAX),
+			@ErrorState VARCHAR(MAX),
+			@ErrorSeverity VARCHAR(MAX),
+			@ErrorLine VARCHAR(MAX),
+			@ErrorMessage VARCHAR(MAX),
+			@ErrorDate DATETIME
+
+	BEGIN TRY 
+		
+		IF NOT EXISTS (SELECT 1 FROM Contacts.Emails WITH(NOLOCK) WHERE EmailDescription = @Email)
+		BEGIN
+			
+			SET @IsActive = 1
+
+			INSERT INTO Contacts.Phones 
+			(
+				PhoneID, 
+				PhoneNumber, 
+				PhoneTypeIDFK, 
+				PhoneIsActive, 
+				UpdatedDate
+			)
+			VALUES(@PhoneID, @PhoneNumber, ISNULL(CONVERT(VARCHAR,@PhoneTypeID), 1), @IsActive, @DefaultDate)
+
+			INSERT INTO Contacts.Emails
+			(
+				EmailID, 
+				EmailDescription, 
+				EmailTypeIDFK, 
+				EmailIsActive, 
+				UpdatedDate
+			)
+			VALUES(@EmailID, @Email, ISNULL(CONVERT(VARCHAR,@EmailType), 1), @IsActive, @DefaultDate)
+
+			
+
+			SET @Message = ''
+
+		END ELSE
+		BEGIN
+			
+			SET @Message = 'Email already exists '
+
+		END
+
+
+		IF (@Message = '' OR @Message = NULL)
+		BEGIN
+			
+			SET @IsActive = 1
+
+			INSERT INTO Contacts.Contacts
+			(
+				ContactID, 
+				PhoneIDFK, 
+				EmailIDFK, 
+				ContactTypeIDFK, 
+				ContactIsActive, 
+				UpdatedDate
+			)
+			VALUES(@ContactID, @PhoneID, @EmailID, @ContactTypeID, @IsActive, @DefaultDate)
+		END
+
+	END TRY
+	BEGIN CATCH
+
+		SET @ErrorSchema = OBJECT_SCHEMA_NAME(@@PROCID)
+			SET @ErrorProc = OBJECT_NAME(@@PROCID)
+			SET @ErrorNumber = ERROR_NUMBER()
+			SET @ErrorState = ERROR_STATE()
+			SET @ErrorSeverity = ERROR_SEVERITY()
+			SET @ErrorLine = ERROR_LINE()
+			SET @ErrorMessage = ERROR_MESSAGE()
+
+			EXEC Auth.spLogExceptions @ErrorSchema,@ErrorProc,@ErrorNumber,@ErrorState,@ErrorSeverity,@ErrorLine,@ErrorMessage,@DefaultDate,32
+
+			SET @Message = (SELECT UserNotification FROM Auth.UserNotification WITH(NOLOCK) WHERE UserNotificationID = 32)
+	END CATCH
+
+SET NOCOUNT OFF
+
+END
+GO
+
+
